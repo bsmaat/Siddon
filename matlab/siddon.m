c1cd7d1749f5d669fb36366932452bb055f2ba7b
Billy <bsmaat@gmail.com>
2017-05-08 00:02:55 +0100

    updated for matlab

--- matlab/siddon.m
+++ matlab/siddon.m
@@ -1,11 +1,11 @@
-
+drawTheGrid = true;
 
 % the points
-p1x = -6;
-p1y = -6;
+p1x = -3;
+p1y = 0;
 
-p2x = 6;
-p2y = 6;
+p2x = 3;
+p2y = 3;
 
 p1 = [p1x p1y];
 p2 = [p2x p2y];
@@ -20,6 +20,34 @@ d = 1;
 % position of first plane (in x or y)
 X0 = -2;
 Y0 = -2;
+
+
+if (drawTheGrid == true) 
+% draw the grid
+
+    % edges
+    X_start = X0;
+    X_end = X0 + (Nx-1) * d;
+    
+    Y_start = Y0;
+    Y_end = Y0 + (Ny-1) * d;
+
+    figure;
+    hold on;
+    for i = 1:Nx
+        for j = 1:Ny
+            Xb = X0 + (i-1)*d;
+            Yb = Y0 + (j-1)*d;
+            line([Xb Xb], [Y_start Y_end]);
+            line([X_start X_end], [Yb Yb]);
+        end
+    end
+
+
+    %plot(A(1),A(2),'o');
+    %plot(B(1),B(2),'*');
+    plot([p1x p2x], [p1y p2y], 'r');
+end
 
 % find alpha_min and alpha_max
 alpha_x0 = (X0 - p1x)/(p2x - p1x);
@@ -42,11 +70,11 @@ alpha_max = min([alpha_xmax, alpha_ymax]);
 
 
 
-# this  is computing imin, imax, jmin, jmax
-# need to find intersection alpha values
+%# this  is computing imin, imax, jmin, jmax
+%# need to find intersection alpha values
 if (alpha_min < alpha_max) 
     disp('Intersecting voxel space...');
-endif
+end
 
 if (p1x < p2x) 
   
@@ -54,13 +82,13 @@ if (p1x < p2x)
     i_min = 1;
   else 
     i_min = ceil((p1x + alpha_min *  (p2x - p1x) - X0)/d);
-  endif
+  end
   
   if (alpha_max == alpha_xmax) 
     i_max = Nx - 1;
   else
     i_max = floor((p1x + alpha_max * (p2x - p1x) - X0)/d);
-  endif
+  end
   
   alphax = ((X0 + i_min * d) - p1x)/(p2x - p1x);
   
@@ -70,19 +98,20 @@ elseif (p1x > p2x)
     i_max = Nx - 2;
   else 
     i_max = floor((p1x + alpha_min * (p2x - p1x) - X0)/d);
-  endif
+  end
   
   if (alpha_max == alpha_xmax) 
     i_min = 0;
   else
     i_min = ceil((p1x + alpha_max * (p2x - p1x) - X0)/d);
-  endif
+  end
   
   alphax = ((X0 + i_max * d) - p1x)/(p2x - p1x);
   
 else 
   disp('Special case: p1x and p2x are the same');
-endif
+end
+
 
 
 if (p1y < p2y) 
@@ -91,13 +120,13 @@ if (p1y < p2y)
     j_min = 1;
   else 
     j_min = ceil((p1y + alpha_min *  (p2y - p1y) - Y0)/d);
-  endif
+  end
   
   if (alpha_max == alpha_ymax) 
     j_max = Ny - 1;
   else
     j_max = floor((p1y + alpha_max * (p2y - p1y) - Y0)/d);
-  endif
+  end
   
   alphay = ((Y0 + j_min * d) - p1y)/(p2y - p1y);
 
@@ -108,22 +137,22 @@ elseif (p1y > p2y)
     j_max = Ny - 2;
   else 
     j_max = floor((p1y + alpha_min * (p2y - p1y) - Y0)/d);
-  endif
+  end
   
   if (alpha_max == alpha_ymax) 
     j_min = 0;
   else
     j_min = ceil((p1y + alpha_max * (p2y - p1y) - Y0)/d);
-  endif
+  end
  
   alphay = ((Y0 + j_max * d) - p1y)/(p2y - p1y);
 
  
 else 
   disp('Special case: p1y and p2y are the same');
-endif
-
-# calculate first interected pixel indices
+end
+    
+%# calculate first interected pixel indices
 
 alphatmp = (min(alphax, alphay) + alpha_min)/2;
 
@@ -138,7 +167,7 @@ elseif (p1x > p2x)
   iu = -1;
 elseif (p1x == p2x) 
   disp('Special case!');
-endif
+end
 
 alphayu = abs(Y0 / (p2y - p1y));
 if (p1y < p2y) 
@@ -147,13 +176,13 @@ elseif (p1y > p2y)
   ju = -1;
 elseif (p1y == p2y) 
   disp('Speical case y!');
-endif
+end
 
 
-# finally
+%# finally
 
-Nv = 4; # numer of pixels intersected
-# dconv is from beginning to end
+Nv = 4; %# numer of pixels intersected
+%# dconv is from beginning to end
 dconv = sqrt( (p2x - p1x)^2 + (p2y - p1y)^2 );
 d12 = 0;
 alphac = alpha_min;
@@ -172,8 +201,8 @@ for k = 1:Nv
     j = j + ju;
     alphac = alphay;
     alphay = alphay + alphayu;
-  endif
-  disp(['Indicies: ', num2str(i),' ', num2str(j)]);
+  end
+  disp(['Indicies: ', num2str(i),' ', num2str(j), ' ', num2str(l)]);
 end
 
 disp(['Distance: ', num2str(d12)]);
